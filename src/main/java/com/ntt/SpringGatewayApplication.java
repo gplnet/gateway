85a7f41388f4b5274d31354be782ff6fa244a2de
cberm3o <cberm3o@gmail.com>
2022-07-01 16:13:24 -0500

    first commit

---
+++ src/main/java/com/ntt/SpringGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.ntt;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class SpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApplication.class, args);
+	}
+	
+	
+
+}
